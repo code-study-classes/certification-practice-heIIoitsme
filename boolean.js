e94a53e80b4b50cf84103eec7343825fb9725030
heIIoitsme <55064941+heIIoitsme@users.noreply.github.com>
2024-03-02 09:15:28 +0300

    Update boolean.js

---
+++ boolean.js
@@ -0,0 +1,8 @@
+// 1 -------------------------------------------
+function inPositive(x1) {
+    return InPositive = Boolean(x1 > 0)
+}
+console.log(inPositive(-1))
+
+// 2 -------------------------------------------
+function 
